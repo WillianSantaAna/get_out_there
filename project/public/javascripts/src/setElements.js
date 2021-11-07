d2ce8239459484543e1b097a5f58c487b2becec8
Willian SantaAna <williansantaana@gmail.com>
2021-11-07 23:28:19 +0000

    fix bug in setElements.js
    Remove buttons "Manage Group" and "Running Calendar"

--- project/public/javascripts/src/setElements.js
+++ project/public/javascripts/src/setElements.js
@@ -42,9 +42,9 @@ function setLogo() {
 function setHomePageButtons() {
   if (getLocalStorageUser()) {
     $(".btn-container")
-      .html(`<a href="./circuit.html" class="btn btn-lg btn-outline-light m-3 m-md-4">Start Running</a>
-        <a href="./group.html" class="btn btn-lg btn-outline-light m-3 m-md-4">Manage Group</a>
-        <a href="./calendar.html" class="btn btn-lg btn-outline-light m-3 m-md-4">Running Calendar</a>`);
+      .html(`<a href="./circuit.html" class="btn btn-lg btn-outline-light m-3 m-md-4">Start Running</a>`);
+      /*<a href="./group.html" class="btn btn-lg btn-outline-light m-3 m-md-4">Manage Group</a>
+        <a href="./calendar.html" class="btn btn-lg btn-outline-light m-3 m-md-4">Running Calendar</a>*/
   } else {
     $(".btn-container")
       .html(`<a href="./register.html" class="btn btn-lg btn-outline-light m-3 m-md-4">Register</a>
